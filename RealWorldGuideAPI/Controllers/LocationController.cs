a0f4b5cb67a0af36621b424a7aecf447eee53674
Heather Downing <quorralyne@gmail.com>
2019-03-29 15:05:59 +0100

    Added location controller and test

---
+++ RealWorldGuideAPI/Controllers/LocationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace RealWorldGuideAPI.Controllers
+{
+    public class LocationController : ApiController
+    {
+        // GET: api/Location/Test
+        [HttpGet, Route("api/location/test")]
+        public string Test()
+        {
+            return "Hello NDC!";
+        }
+    }
+}
